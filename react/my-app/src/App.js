80ac7bb6446af51481a615302a6ca4429a20a9e6
TomokiKonishi <9137048584@g.ecc.u-tokyo.ac.jp>
2025-11-30 10:15:38 +0900

    react課題1-3のコミット

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,17 +1,23 @@
-import logo from './logo.svg';
+import logo from "./logo.svg";
+import "./App.css";
 import { useState } from "react";
-import './App.css';
 
 function App() {
-  const [isVisible, setIsVisible] = useState(true);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [tag,setTag]= useState("");
+
+  const handleSave = () => {
+    // 現在のState（title と content）をオブジェクトとしてコンソールに出力
+    console.log("保存するメモ: ", { title, content,tag });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" 
-        onClick={() => console.log('ロゴがクリックされました')}
-        style={{ cursor: 'pointer' }}/>
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to react!
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -21,15 +27,33 @@ function App() {
         >
           Learn React
         </a>
-     
-      <button onClick={() => setIsVisible(!isVisible)}>
-  Toggle Text
-</button>
-{isVisible && <p>このテキストが表示/非表示されます</p>}
- </header>
+      </header>
+      {/* メモ帳のフォーム */}
+      <div style={{ display: "flex", flexDirection: "column", padding: 20 }}>
+        <label>タイトル: </label>
+        <input
+          type={"text"}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          style={{ marginBottom: 10 }}
+        />
+        <label>内容: </label>
+        <textarea
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          style={{ marginBottom: 20, height: 80 }}
+        />
+
+         <label>タグ: </label>
+        <input
+          type={"text"}
+          value={tag}
+          onChange={(e) => setTag(e.target.value)}
+          style={{ marginBottom: 10 }}
+        />
+        <button onClick={handleSave}>保存</button>
+      </div>
     </div>
-    
-    
   );
 }
 
